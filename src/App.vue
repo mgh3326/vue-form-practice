3d1e75da194822dd4ae656d47903806d654b2a4e
kwanghyunMoon <mgh3326@naver.com>
2019-07-29 11:58:29 +0900

    update complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form v-on:submit.prevent="submitForm">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" id="username" v-model="username">
+    </div>
+    <div>
+      <label for="password">password: </label>
+      <input type="text" id="password" v-model="password">
+    </div>
+    <button type="submit">login</button>
+  </form>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  import * as axios from "axios";
+
+  export default {
+    name: 'app',
+    data: function () {
+      return {
+        username: '',
+        password: '',
+      }
+    },
+    methods: {
+      submitForm: function () {
+        console.log((this.username + " " + this.password));
+        var url = 'https://jsonplaceholder.typicode.com/users';
+        var data = {
+          username: this.username,
+          password: this.password
+        };
+        axios.post(url, data).then(
+          function (res) {
+            console.log(res);
+
+          }
+        ).catch(function (err) {
+          console.log(err);
+        });
+      }
+    }
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
